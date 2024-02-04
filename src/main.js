4764417a9de289b760dde598b46939e17884cb42
avezqureshi14 <aveznqureshi14@gmail.com>
2024-02-04 14:54:18 +0530

    feat:input_schema.json updated

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import store from "app-store-scraper";
-import { collection } from "./constants/collection.js";
 import { category } from "./constants/category.js";
 
 import { Actor } from "apify";
@@ -32,31 +31,25 @@ const runActor = async () => {
   await Actor.exit();
 };
 
-const listApps = async ({ selectedCategory,popularity, limit, priceModel }) => {
+const listApps = async ({ selectedCategory,limit, priceModel }) => {
   const appStoreCategory = category[selectedCategory];
-  const appStoreCollection = collection[popularity];
 
   try {
     const allApps = await store.list({
-      collection: appStoreCollection,
       category: appStoreCategory,
       num: limit
     });
 
     let filteredApps = allApps;
 
-    // if (priceModel === "FREE") {
-    //   // Filter apps to include only free apps (Freemium, Ad-Supported, etc.)
-    //   filteredApps = filteredApps.filter((app) => app.free === true);
-    // } else if (priceModel === "PAID") {
-    //   // Filter apps to include only paid apps
-    //   filteredApps = filteredApps.filter((app) => app.free === false);
-    // }
+    if (priceModel === "FREE") {
+      filteredApps = filteredApps.filter((app) => app.free === true);
+    } else if (priceModel === "PAID") {
+      filteredApps = filteredApps.filter((app) => app.free === false);
+    }
     await Actor.pushData(filteredApps.slice(0, limit));
-    // Use Actor.pushData as needed
   } catch (error) {
     console.error("Error fetching data from App Store:", error);
-    // Handle the error or use Actor.pushData as needed
     await Actor.pushData({error:error.message});
 
   }
